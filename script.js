32a1fd3736b1757c427b4afb888333918b057c59
George Tinsley <george8s@live.co.uk>
2023-08-15 11:55:08 +0100

    added script consts and pointer for boxes

---
+++ script.js
@@ -0,0 +1,15 @@
+const soundBtns = document.getElementsByClassName("soundBtns");
+const boom = document.getElementById("boom");
+const clap = document.getElementById("clap");
+const hihat = document.getElementById("hihat");
+const kick = document.getElementById("kick");
+const openhat = document.getElementById("openhat");
+const ride = document.getElementById("ride");
+const snare = document.getElementById("snare");
+const tink = document.getElementById("tink");
+const tom = document.getElementById("tom");
+
+
+// soundBtns.addEventListener("click", () => {
+
+// })
