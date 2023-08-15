e233220ed3c3e8bb53bd4f24766b5f432206acd3
George Tinsley <george8s@live.co.uk>
2023-08-15 12:33:55 +0100

    added sounds on click, foundation code added for onpress

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const soundBtns = document.getElementsByClassName("soundBtns");
+const soundBtns = document.getElementById("soundBtns");
 const boom = document.getElementById("boom");
 const clap = document.getElementById("clap");
 const hihat = document.getElementById("hihat");
@@ -9,7 +9,79 @@ const snare = document.getElementById("snare");
 const tink = document.getElementById("tink");
 const tom = document.getElementById("tom");
 
+const boomAudio = document.getElementById("boomAudio");
+const clapAudio = document.getElementById("clapAudio");
+const hihatAudio = document.getElementById("hihatAudio");
+const kickAudio = document.getElementById("kickAudio");
+const openhatAudio = document.getElementById("openhatAudio");
+const rideAudio = document.getElementById("rideAudio");
+const snareAudio = document.getElementById("snareAudio");
+const tinkAudio = document.getElementById("tinkAudio");
+const tomAudio = document.getElementById("tomAudio");
 
-// soundBtns.addEventListener("click", () => {
+// soundBtns.array.forEach((letterSound) => {
+//     letterSound.addEventListener("click", (e) => {
 
+//     })
+// });
+
+boom.addEventListener("click", () => {
+    boomAudio.load();
+    boomAudio.play();
+})
+
+clap.addEventListener("click", () => {
+    clapAudio.load();
+    clapAudio.play();
+})
+hihat.addEventListener("click", () => {
+    hihatAudio.load();
+    hihatAudio.play();
+})
+kick.addEventListener("click", () => {
+    kickAudio.load();
+    kickAudio.play();
+})
+openhat.addEventListener("click", () => {
+    openhatAudio.load();
+    openhatAudio.play();
+})
+ride.addEventListener("click", () => {
+    rideAudio.load();
+    rideAudio.play();
+})
+snare.addEventListener("click", () => {
+    snareAudio.load();
+    snareAudio.play();
+})
+tink.addEventListener("click", () => {
+    tinkAudio.load();
+    tinkAudio.play();
+})
+tom.addEventListener("click", () => {
+    tomAudio.load();
+    tomAudio.play();
+})
+
+// document.addEventListener("keyPressed", () => {
+//     if (keyPressed == "a" || keyPressed == "A"){
+//     boomAudio.play();
+//     } else if (keyPressed == "s" || keyPressed == "S"){
+//         clapAudio.play();
+//     }else if (keyPressed == "d" || keyPressed == "D"){
+//         hihatAudio.play();
+//     }else if (keyPressed == "f" || keyPressed == "F"){
+//         kickAudio.play();
+//     }else if (keyPressed == "g" || keyPressed == "G"){
+//         openhatAudio.play();
+//     }else if (keyPressed == "h" || keyPressed == "H"){
+//         rideAudio.play();
+//     }else if (keyPressed == "j" || keyPressed == "J"){
+//         snareAudio.play();
+//     }else if (keyPressed == "k" || keyPressed == "K"){
+//         tinkAudio.play();
+//     }else if (keyPressed == "l" || keyPressed == "L"){
+//         tomAudio.play();
+//     }
 // })
+
